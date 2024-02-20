a893a7e2dc952b4665b4dddcdce9231ee5f9fcb2
Dmytro Hyriavets <hyriavets.dm@gmail.com>
2024-02-20 16:52:15 +0200

    On branch upd_back_2  Changes to be committed: 	new file:   ExpressTS/src/middleware/auth_middleware.ts 	new file:   ExpressTS/src/middleware/error_middleware.ts 	new file:   ExpressTS/src/middleware/logger_midddleware.ts

---
+++ ExpressTS/src/middleware/error_middleware.ts
@@ -0,0 +1,13 @@
+import { NextFunction, Request, Response } from 'express';
+import {BaseHttpError} from '../util/errors';
+
+export default function errorMiddleware(error: BaseHttpError, request: Request, response: Response, next: NextFunction) {
+  const status = error.status || 500;
+  const message = error.message || 'Something went wrong';
+  response
+    .status(status)
+    .send({
+      message,
+      status,
+    });
+}
